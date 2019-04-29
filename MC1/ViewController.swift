2931364690a4cfeca09635540eeed9e32a542498
Adrian <abiyoga98@gmail.com>
2019-04-29 22:04:12 +0700

    Initial Commit

---
+++ MC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC1
+//
+//  Created by Adrian Suryo Abiyoga on 29/04/19.
+//  Copyright © 2019 Adrian Suryo Abiyoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
