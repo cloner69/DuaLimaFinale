e0a298c388ccf5da6d6225f6cdf53511f0d89dd7
Adrian <abiyoga98@gmail.com>
2019-04-30 14:00:40 +0700

    first commit

---
+++ MC1/nextViewController.swift
@@ -0,0 +1,9 @@
+//
+//  nextViewController.swift
+//  MC1
+//
+//  Created by Adrian Suryo Abiyoga on 29/04/19.
+//  Copyright © 2019 Adrian Suryo Abiyoga. All rights reserved.
+//
+
+import Foundation
